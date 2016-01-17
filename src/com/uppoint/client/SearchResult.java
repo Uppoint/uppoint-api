b452996c06b8ed0597edbabf6ff9905d9a26cfdc
dalexiev <dalexiev@gmail.com>
2016-01-17 15:25:38 +0100

    Added the user defined services to the search results

--- src/com/uppoint/client/SearchResult.java
+++ src/com/uppoint/client/SearchResult.java
@@ -1,10 +1,14 @@
 package com.uppoint.client;
 
+import java.util.List;
+
 import com.uppoint.core.model.ProUser;
+import com.uppoint.core.model.UserDefinedService;
 
 class SearchResult {
 
 	private ProUser user;
+	private List<UserDefinedService> services;
 
 	public ProUser getUser() {
 		return user;
@@ -12,6 +16,14 @@ class SearchResult {
 
 	public void setUser(ProUser user) {
 		this.user = user;
+	}
+
+	public List<UserDefinedService> getServices() {
+		return services;
+	}
+
+	public void setServices(List<UserDefinedService> services) {
+		this.services = services;
 	}
 
 }
--- src/com/uppoint/client/SearchService.java
+++ src/com/uppoint/client/SearchService.java
@@ -14,38 +14,58 @@ import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.uppoint.core.mapper.ProUserMapper;
+import com.uppoint.core.mapper.UserDefinedServiceMapper;
 import com.uppoint.core.model.ProUser;
+import com.uppoint.core.model.UserDefinedService;
 
 class SearchService {
 
 	private final DatastoreService datastore;
 	private final ProUserMapper proUserMapper;
+	private final UserDefinedServiceMapper serviceMapper;
 
 	SearchService() {
 		this.datastore = DatastoreServiceFactory.getDatastoreService();
 		this.proUserMapper = new ProUserMapper();
+		this.serviceMapper = new UserDefinedServiceMapper();
 	}
 
 	SearchResults search(Key countryKey, Key cityKey, Key professionKey, Key serviceTypeKey) {
 		final Query query = new Query(ProUser.SearchIndex.KIND)
 				.setFilter(createSearchIndexFilter(countryKey, cityKey, professionKey, serviceTypeKey)).setKeysOnly();
 		final Iterator<Entity> resultIter = this.datastore.prepare(query).asIterator();
-		final List<SearchResult> searchResultList = mapQueryResults(resultIter);
+		final List<SearchResult> searchResultList = mapQueryResults(resultIter, serviceTypeKey);
 		final SearchResults searchResults = new SearchResults();
 		searchResults.setResults(searchResultList);
 		return searchResults;
 	}
 
-	private List<SearchResult> mapQueryResults(final Iterator<Entity> resultIter) {
+	private List<SearchResult> mapQueryResults(final Iterator<Entity> resultIter, Key serviceTypeKey) {
 		final Map<Key, Entity> userEntities = fetchResultUsers(resultIter);
 		final List<SearchResult> searchResultList = new LinkedList<>();
 		for (Entity userEntity : userEntities.values()) {
 			final ProUser user = this.proUserMapper.toModel(userEntity);
+
 			final SearchResult result = new SearchResult();
 			result.setUser(user);
+
+			final List<UserDefinedService> services = fetchServicesForUser(userEntity, serviceTypeKey);
+			result.setServices(services);
+
 			searchResultList.add(result);
 		}
 		return searchResultList;
+	}
+
+	private List<UserDefinedService> fetchServicesForUser(Entity userEntity, Key serviceTypeKey) {
+		final Query query = new Query(UserDefinedService.class.getSimpleName(), userEntity.getKey())
+				.setFilter(new Query.FilterPredicate("serviceTypeKey", FilterOperator.EQUAL, serviceTypeKey));
+		final Iterator<Entity> serviceIter = this.datastore.prepare(query).asIterator();
+		final List<UserDefinedService> services = new LinkedList<>();
+		while (serviceIter.hasNext()) {
+			services.add(this.serviceMapper.toModel(serviceIter.next()));
+		}
+		return services;
 	}
 
 	private Map<Key, Entity> fetchResultUsers(final Iterator<Entity> resultIter) {
