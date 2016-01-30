8f9d53904264eae3e0dca60d3306bd9ea6341064
dalexiev <dalexiev@gmail.com>
2016-01-30 14:16:42 +0100

    Added the newly generated iOS client ID

--- src/com/uppoint/core/Constants.java
+++ src/com/uppoint/core/Constants.java
@@ -8,7 +8,7 @@ import com.google.api.server.spi.Constant;
 public final class Constants {
   public static final String WEB_CLIENT_ID = "55945210201-njqenms4v43b16d3pdatkntknra8d0h4.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
-  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String IOS_CLIENT_ID = "55945210201-d8qpiorvg5cvh7e61nafsv8vddqna8uj.apps.googleusercontent.com";
   public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 
